78a9b090b6c4b3d0c3257bcaf4fd472777ef360b
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2022-07-20 12:23:45 +0200

    script for creating user

---
+++ 06_user.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -x #echo on
+
+# This is meant to be executed with arch-chroot /mnt
+# and it has to be copied inside /mnt first
+# example, after copying it to /mnt/root 
+# arch-chroot /mnt /root/06_user.sh
+
+useradd -m bettini
+
+usermod -aG wheel,sys,rfkill bettini
+
+sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+echo -ne "
+IMPORTANT: remember to set the password in the end!
+"
