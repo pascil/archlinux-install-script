6662df25c88a7ca6c185189ffd17e2fb205d5783
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2022-07-20 15:28:40 +0200

    main install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -x #echo on
+
+echo -ne "
+Starting...
+"
+
+( ./00_check.sh )|& tee 00_check.log
+( ./01_mount-partitions.sh )|& tee 01_mount-partitions.log
+( ./02_pacstrap.sh )|& tee 02_pacstrap.log
+( ./03_prepare-for-arch-chroot.sh )|& tee 03_prepare-for-arch-chroot.log
+( arch-chroot /mnt /root/04_configuration.sh )|& tee 04_configuration.log
+( arch-chroot /mnt /root/05_bootloader.sh )|& tee 05_bootloader.log
+( arch-chroot /mnt /root/06_user.sh )|& tee 06_user.log
+cp -v *.log /mnt/home/bettini/
+
+echo -ne "
+...Finished!
+"
