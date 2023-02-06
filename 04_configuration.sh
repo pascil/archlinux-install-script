9acb6700000069ca4c6f66f9ae23c35fa1bf246d
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-02-06 10:49:49 +0100

    better locale.conf

--- 04_configuration.sh
+++ 04_configuration.sh
@@ -17,7 +17,18 @@ echo it_IT.UTF-8 UTF-8 >> /etc/locale.gen
 
 locale-gen
 
-echo LANG=en_US.UTF-8 >> /etc/locale.conf
+cat >> /etc/locale.conf << EOF
+LANG=en_US.UTF-8
+LC_ADDRESS=it_IT.UTF-8
+LC_IDENTIFICATION=it_IT.UTF-8
+LC_MEASUREMENT=it_IT.UTF-8
+LC_MONETARY=it_IT.UTF-8
+LC_NAME=it_IT.UTF-8
+LC_NUMERIC=it_IT.UTF-8
+LC_PAPER=it_IT.UTF-8
+LC_TELEPHONE=it_IT.UTF-8
+LC_TIME=it_IT.UTF-8
+EOF
 
 echo KEYMAP=it >> /etc/vconsole.conf
 
